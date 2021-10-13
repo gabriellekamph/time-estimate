857fc47c92084ee632d7db8518f9562dfff9b2d4
Isak Antin <isakantin@Isaks-MacBook-Air.local>
2021-10-13 14:36:30 +0200

    added git API with axios package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,66 @@
-import './App.css';
+import React from 'react';
+import axios, { CancelTokenSource } from 'axios';
 import Form from './components/Form';
+
+interface IPost {
+  id: number;
+  title: string;
+}
+
+const defaultPosts: IPost[] = [];
+
+const App = () => {
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(
+    defaultPosts
+  );
+
+  const [loading, setLoading]: [
+    boolean,
+    (loading: boolean) => void
+  ] = React.useState<boolean>(true);
+
+  const [error, setError]: [string, (error: string) => void] = React.useState(
+    ''
+  );
+
+  const cancelToken = axios.CancelToken; //create cancel token
+  const [cancelTokenSource, setCancelTokenSource]: [
+    CancelTokenSource,
+    (cancelTokenSource: CancelTokenSource) => void
+  ] = React.useState(cancelToken.source());
+
+  const handleCancelClick = () => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel('User cancelled operation');
+    }
+  };
+
+  React.useEffect(() => {
+    axios
+      .get<IPost[]>('https://api.github.com/repos/Ant1N/time-estimate/issues', {
+        cancelToken: cancelTokenSource.token,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        timeout: 10000,
+      })
+      .then((response) => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch((ex) => {
+        let error = axios.isCancel(ex)
+          ? 'Request Cancelled'
+          : ex.code === 'ECONNABORTED'
+          ? 'A timeout has occurred'
+          : ex.response.status === 404
+          ? 'Resource Not Found'
+          : 'An unexpected error has occurred';
+
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
 
   // Function to handle when new estimated is saved (after button click)
 
@@ -7,13 +68,20 @@ import Form from './components/Form';
     console.log("Estimate (not yet) saved! (but it's supposed to when this function is done :))");
   }
 
-function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
-      <Form estimate={1} handleSaveEstimate={handleSaveEstimate} />
-    </div>
-  );
+<h1>Hello!</h1>
+<Form estimate={1} handleSaveEstimate={handleSaveEstimate} />
+      <ul className="posts">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+          </li>
+        ))}
+      </ul> 
+      </div>
+);
 }
 
 export default App;
+
